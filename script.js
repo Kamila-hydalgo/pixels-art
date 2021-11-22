ad7890c8f2c1e38da2a4d28b8ba74d53bf7a54bf
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-11-22 19:52:26 -0300

    Requisito 7 realizado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Requisito 4
 const pixelBoard = document.getElementById('pixel-board');
 const lines = 5;
 const coluns = 5;
@@ -10,5 +11,26 @@ for (let index = 1; index <= lines; index += 1) {
   }
   pixelBoard.appendChild(divLine);
 }
+
+// Requisito 6
 const selected = document.getElementById('black');
 selected.classList.add('selected');
+
+// Requisito 7
+const firstColor = document.getElementById('black');
+const secondColor = document.getElementById('green');
+const thirdColor = document.getElementById('blue');
+const fourthColor = document.getElementById('pink');
+
+firstColor.addEventListener('click', selectColor);
+secondColor.addEventListener('click', selectColor);
+thirdColor.addEventListener('click', selectColor);
+fourthColor.addEventListener('click', selectColor);
+
+function selectColor(event) {
+  const colorSelected = document.querySelector('.selected');
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+// Requisito 8
