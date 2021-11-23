3d9a073312f1e0d35996b28dd980dd2f629bc541
Kamila Hydalgo <kamilahydalgo@gmail.com>
2021-11-23 00:48:28 -0300

    Requisitos 8 e 12 feitos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Requisito 2 e 3
+let firstColor = document.querySelectorAll('.color')[0];
+firstColor.style.backgroundColor = 'black';
+
+let secondColor = document.querySelectorAll('.color')[1];
+secondColor.style.backgroundColor = 'green';
+
+let thirdColor = document.querySelectorAll('.color')[2];
+thirdColor.style.backgroundColor = 'blue';
+
+let fourthColor = document.querySelectorAll('.color')[3];
+fourthColor.style.backgroundColor = 'pink';
+
 // Requisito 4
 const pixelBoard = document.getElementById('pixel-board');
 const lines = 5;
@@ -13,24 +26,45 @@ for (let index = 1; index <= lines; index += 1) {
 }
 
 // Requisito 6
-const selected = document.getElementById('black');
+const selected = document.querySelectorAll('.color')[0];
 selected.classList.add('selected');
 
 // Requisito 7
-const firstColor = document.getElementById('black');
-const secondColor = document.getElementById('green');
-const thirdColor = document.getElementById('blue');
-const fourthColor = document.getElementById('pink');
-
-firstColor.addEventListener('click', selectColor);
-secondColor.addEventListener('click', selectColor);
-thirdColor.addEventListener('click', selectColor);
-fourthColor.addEventListener('click', selectColor);
-
 function selectColor(event) {
   const colorSelected = document.querySelector('.selected');
   colorSelected.classList.remove('selected');
   event.target.classList.add('selected');
 }
+firstColor.addEventListener('click', selectColor);
+secondColor.addEventListener('click', selectColor);
+thirdColor.addEventListener('click', selectColor);
+fourthColor.addEventListener('click', selectColor);
 
 // Requisito 8
+
+const pixelSelected = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixelSelected.length; index += 1) {
+  pixelSelected[index].addEventListener('click', paintPixel);
+}
+
+function paintPixel(event) {
+  const colorSelected = document.querySelector('.selected');
+  let addColor = colorSelected.style.backgroundColor;
+  event.target.style.backgroundColor = addColor;
+}
+
+// Requisito 12
+// Referência
+function randomColor() {
+  const aleatColor1 = Math.round(Math.random() * 255);
+  const aleatColor2 = Math.round(Math.random() * 255);
+  const aleatColor3 = Math.round(Math.random() * 255);
+  const newColor = `rgb(${aleatColor1},${aleatColor2},${aleatColor3})`;
+  return newColor;
+}
+
+secondColor.style.backgroundColor = randomColor();
+
+thirdColor.style.backgroundColor = randomColor();
+
+// fourthColor.style.backgroundColor = randomColor();
